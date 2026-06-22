1c2edff4b2a8bc2b4b24bb5d4b9a8e006fc19dfe
Daniel Mestre dos Sapos e outras coisas <daniel.sobrinho1989@gmail.com>
2026-06-21 23:33:11 -0300

    Ajustando build

--- buildozer.spec
+++ buildozer.spec
@@ -194,7 +194,7 @@ android.accept_sdk_license = True
 # Either form may be used, and assets need not be in 'source.include_exts'.
 # 1) android.add_assets = source_asset_relative_path
 # 2) android.add_assets = source_asset_path:destination_asset_relative_path
-android.add_assets = domains/conversas/qwen2.5-1.5b-instruct-q8_0.gguf
+#android.add_assets = 
 
 # (list) Put these files or directories in the apk res directory.
 # The option may be used in three ways, the value may contain one or zero ':'
--- domains/conversas/qwen_local_client.py
+++ domains/conversas/qwen_local_client.py
@@ -5,19 +5,27 @@ IS_ANDROID = "ANDROID_ARGUMENT" in os.environ
 if IS_ANDROID:
     from jnius import autoclass
 
+    from android.storage import app_storage_path
+
     QwenBridge = autoclass("domains.voz.java.QwenBridge")
 
     class QwenLocalClient:
         def __init__(self):
             self.bridge = QwenBridge()
 
-            model_path = (
-                "/data/data/"
-                "br.com.saposapudo.saposapudo/"
-                "files/app/"
-                "domains/conversas/"
-                "qwen2.5-1.5b-instruct-q8_0.gguf"
+            base_path = app_storage_path()
+
+            model_path = os.path.join(
+                base_path,
+                "app",
+                "domains",
+                "conversas",
+                "qwen2.5-1.5b-instruct-q8_0.gguf",
             )
+
+            print("APP_STORAGE =", base_path)
+            print("QWEN_PATH =", model_path)
+            print("QWEN_EXISTS =", os.path.exists(model_path))
 
             if not os.path.exists(model_path):
                 raise RuntimeError(f"Modelo não encontrado: {model_path}")
@@ -31,6 +39,7 @@ if IS_ANDROID:
             texto = self.bridge.generate(system, prompt)
 
             return {"texto": texto.strip() if texto else "", "arquivo_audio": None}
+
 else:
 
     class QwenLocalClient:
